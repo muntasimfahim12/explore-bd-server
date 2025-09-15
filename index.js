7379806cc2d156ddee3d6748f4e73e1c88051286
muntasimfahim12 <fahimmuntasim192@gmail.com>
2025-09-15 13:49:22 -0700

    git push

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://expolre-bd.vercel.app", // or "http://127.0.0.1:3000"
+  })
+);
+
+
 app.use(express.json());
 
 
